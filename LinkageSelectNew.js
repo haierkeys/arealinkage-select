c5775cdc5557c6cb7053031101150064cc114f66
haierspi <haierspi@qq.com>
2015-12-01 19:28:13 +0800

    111

--- LinkageSelectNew.js
+++ LinkageSelectNew.js
@@ -17,30 +17,54 @@ function LinkageSelect(options) {
 	if(options) {  
 		jQuery.extend(settings, options); 
 	}
+
+	function build(province,city,district){
+		var province_options = '<option value="0">省份/自治区</option>';
+		var city_options = '<option value="0">城市/地区</option>';
+		var district_options = '<option value="0">区/县</option>';
+		var opt,opt_title,opt_value,opt_selected;
+		for(var key1 in settings.data) {
+			opt1 = settings.data[key1];
+			opt_title	= opt1['name'];
+			opt_value	= opt1['id'];
+			opt_selected = province == opt_value?'selected="selected"':''
+			province_options += '<option value="' + opt_value + '" ' + opt_selected + '>' + opt_title + '</option>';
+
+			if (province == opt_value) {
+				for(var key2 in opt1['child']) {
+
+					opt2 = opt1['child'][key2];
+					opt_title	= opt2['name'];
+					opt_value	= opt2['id'];
+					opt_selected = city == opt_value?'selected="selected"':''
+					city_options += '<option value="' + opt_value + '" ' + opt_selected + '>' + opt_title + '</option>';
+
+					if (city == opt_value) {
+						for(var key3 in opt2['child']) {
+
+							opt3 = opt2['child'][key3];
+							opt_title	= opt3['name'];
+							opt_value	= opt3['id'];
+							opt_selected = district == opt_value?'selected="selected"':''
+							district_options += '<option value="' + opt_value + '" ' + opt_selected + '>' + opt_title + '</option>';
+						}
+					};
+				}
+			};
+		}
+		$(settings.province).html(province_options)
+		$(settings.city).html(city_options);
+		$(settings.district).html(district_options);
+	}
 	
 	function _init() {
-
-		$(settings.province).append('<option value="0">省份/自治区</option>');
-		$(settings.city).append('<option value="0">城市/地区</option>');
-		$(settings.district).append('<option value="0">区/县</option>');
-
-		console.log(settings.data);
-
-		for(var opt_value in settings.data) {
-
-
-			var opt_title	= settings.data[opt_value]['name'];
-			var selected	= '';
-			// if (opt_value == value) {
-			// 	selected_index	= index;
-			// 	selected		= 'selected="selected"';
-			// }
-			var option	= $('<option value="' + opt_value + '" ' + selected + '>' + opt_title + '</option>');
-			$(settings.province).append(option);
-			index++;
-		}
-
+		
+		$(settings.province+', '+settings.city).change(function(){
+			province = $(settings.province).find("option:selected").val();
+			city = $(settings.city).find("option:selected").val();
+			build(province,city);
+		});
+		build(settings.provinceset,settings.cityset,settings.districtset);
 	}
 	_init();
-	
 }
